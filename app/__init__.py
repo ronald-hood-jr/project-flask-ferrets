76e8a1eb9ca4cc7572a1bdd20aba8afda3cd0d97
mlam21 <mlam21@students.cs.ubc.ca>
2022-06-02 21:34:27 -0700

    added menu bar using jinja and bootstrtrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,3 +9,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+@app.route('/experiences')
+def experiences():
+    return render_template('experiences.html')
+
