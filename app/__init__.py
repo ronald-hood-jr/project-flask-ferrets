2f83f42eb13616ec5f14de70aa524b59469c6e92
Ronald Hood Jr <ronaldhoodjr@gmail.com>
2022-06-27 01:42:00 -0400

    timeline

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,26 @@ import os
 import json
 from flask import Flask, render_template, request, url_for
 from dotenv import load_dotenv
+from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
+import requests
+from requests.structures import CaseInsensitiveDict
+url = "http://localhost:5000/api/timeline_post"
+
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306)
+
+print(mydb)
 
 
 @app.route('/')
@@ -26,6 +43,47 @@ def experiences():
     experiences = get_static_json("static/files/experiences.json")
     return render_template('experiences.html', experiences=experiences)
 
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+@app.route('/timeline')
+def timeline():
+    
+    timeline = {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+    return render_template('timeline.html', timeline=timeline)
+
+@app.route('/timeline')
+def timeline_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    
+
+def createJSON():
+    resp = "h"
+    f = open("static/files/timeline.json", "w")
+    f.write(resp)
+    f.close()
+
 def get_static_json(path):
     return json.load(open(get_static_file(path)))
 
@@ -34,3 +92,13 @@ def get_static_file(path):
     return os.path.join(root, path)
     
 
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
