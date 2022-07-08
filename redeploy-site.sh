6e6f58c712dbc07090602416b70ccbd15ff8ccd9
Ronald Hood Jr <ronaldhoodjr@gmail.com>
2022-07-08 18:20:06 +0000

    redeploy update

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd project-flask-ferrets
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
